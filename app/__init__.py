98bcdd98642bbde2d0f05710ae10031c0ff0c1ce
shyyyyny <ka558@gmail.com>
2024-02-29 22:04:19 -0500

    start and load commands

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,35 @@
-from abc import ABC, abstractmethod
+import os
+from app.commands import CommandHandler
+from app.plugins.menu import MenuCommand
 
-class Command(ABC):
-    @abstractmethod
-    def execute(self, *args, **kwargs):pass
-
-class CommandHandler:
+class App:
     def __init__(self):
-        self.commands = {}
+        self.command_handler = CommandHandler()
+        self._load_commands()
 
-    def register_command(self, command_name: str, command: Command):
-        self.commands[command_name] = command
+    def _load_commands(self):
+        command_dir = "app/plugins" 
+        for subdir in os.listdir(command_dir):
+            subdir_path = os.path.join(command_dir, subdir)
+            if os.path.isdir(subdir_path):
+                # try:
+                    if subdir.lower() == "menu":
+                        command_instance = MenuCommand(self.command_handler)
+                    else:
+                        module_name = f"app.plugins.{subdir}.__init__"
+                        command_class_name = getattr(__import__(module_name, fromlist=[f"{subdir.capitalize()}Command"]), f"{subdir.capitalize()}Command")
+                        command_instance = command_class_name()
+                    self.command_handler.register_command(subdir, command_instance)
+                # except Exception:
+                #     pass
 
-    def execute_command(self, command_name: str, *args, **kwargs):
-        try:
-            # Passing additional arguments to the command's execute method
-            self.commands[command_name].execute(*args, **kwargs)
-        except KeyError:
-            print(f"No such command: {command_name}")
+    def start(self):
+        print("Type 'exit' to exit.")
+        while True:
+            user_input = input(">>> ").strip().split()
+            if not user_input:
+                print("Please enter a command.")
+                continue
+            command_name = user_input[0].lower()
+            args = user_input[1:]
+            self.command_handler.execute_command(command_name, args)
